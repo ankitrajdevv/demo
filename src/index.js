8b319aa0371b799481c375f1c8735fc893d94efc
Ankit Raj <w.ankitrajdev.com>
2024-10-21 23:50:38 +0530

    d

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-// index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
-
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,5 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
-// Register the service worker to enable offline mode
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
 serviceWorkerRegistration.register();
